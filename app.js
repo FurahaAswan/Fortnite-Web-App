6a5a4ffa701babc22f6590749437f2a0646114b2
Furaha Aswan <furaha.aswan@gmail.com>
2018-10-09 16:38:04 -0500

    Fourth Commit

--- app.js
+++ app.js
@@ -1,29 +1,18 @@
 var express 	= require("express"),
 	app 		= express(),
-	request 	= require("request");
+	request 	= require("request"),
+	fs			= require("fs");
 	bodyParser 	= require('body-parser');
 
 app.set("view engine", "ejs");
+app.set("port", (process.env.PORT || 5000));
 app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 var apiKey = {"Authorization": "2e04211c18b48a93110d5ff1f0b06f90"};
 
 app.get("/", function(req,res){
-	var url = "https://fortnite-public-api.theapinetwork.com/prod09/br_motd/get";
-	var options = {
-		headers: apiKey,
-		url: url,
-		form: {"language": "en"}
-	}
-	request.post(options, function(error, response, body){
-		if(!error){
-			res.render("index", {data: JSON.parse(body)});
-		}
-		else{
-			res.send("error");
-		}
-	})
+	res.render("index");
 });
 
 app.post("/stats", function(req,res){
@@ -35,17 +24,52 @@ app.post("/stats", function(req,res){
 		url: url,
 		form: user
 	}
-	// request.post(options, function(error, response, body){
-	// 	console.log(JSON.parse(body));
-	// 	if (typeof body !== 'undefined') {
- // 		 	data = JSON.parse(body);
- // 		 	data.preference = req.body.platform;
+	request.post(options, function(error, response, body){
+		console.log(JSON.parse(body));
+		if (typeof body !== 'undefined') {
+ 		 	data = JSON.parse(body);
+ 		 	data.preference = req.body.platform;
  		 	res.render("stats");
-// 		} else{
-// 			res.send("undefined");
-// 		}
-// 	});
+		} else{
+			res.send("undefined");
+		}
+	});
 });
+
+app.get('/video', function(req, res) {
+  const path = 'public/assets/videos/sample.mp4'
+  const stat = fs.statSync(path)
+  const fileSize = stat.size
+  const range = req.headers.range
+
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-")
+    const start = parseInt(parts[0], 10)
+    const end = parts[1]
+      ? parseInt(parts[1], 10)
+      : fileSize-1
+
+    const chunksize = (end-start)+1
+    const file = fs.createReadStream(path, {start, end})
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    }
+
+    res.writeHead(206, head)
+    file.pipe(res)
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(200, head)
+    fs.createReadStream(path).pipe(res)
+  }
+})
+
 
 app.listen(8000, function(){
 	console.log("the app is running");
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,29 +1,118 @@
-<script>
-	var APIkey = {"Authorization": "2e04211c18b48a93110d5ff1f0b06f90"};
-	$.ajax({
-	  			type: "POST",
-	  			url: "https://fortnite-public-api.theapinetwork.com/prod09/users/id",
-	  			headers: APIkey,
-	  			dataType: "json",
-	  			data: {"username": "FurFur_BEAST"}
-	  		})
-	  		.done(function(data) {
-	  			console.log('GET response:', JSON.stringify(data));
-	  			$.ajax({
-	  				type: "POST",
-	  				url: "https://fortnite-public-api.theapinetwork.com/prod09/users/public/br_stats",
-	  				headers: APIkey,
-	  				dataType: "json",
-	  				data: {user_id: data["uid"], platform: data["platforms"][1], "window": "alltime"},
-	  				success: function(stuff){
-	  					console.log(JSON.stringify(stuff))
-	  				},
-	  				error: function(textStatus, errorThrown){
-	  					console.log(errorThrown);
-	  				}
-	  			})
-	  		})
-	  		.fail(function(jqXHR, textStatus, err) {
-	  			console.log("Ajax error response:", textStatus);
-	  		});
-</script>
+console.log("ahk");
+// compiler("kills");
+// 	compiler("wins");
+// 	compiler("score");
+var APIKey = {"Authorization": "2e04211c18b48a93110d5ff1f0b06f90"};
+// 	function compiler(input){
+// 	$.ajax({
+// 	  			type: "POST",
+// 	  			url: "https://fortnite-public-api.theapinetwork.com/prod09/leaderboards/get",
+// 	  			headers: APIKey,
+// 	  			statsType: "json",
+// 	  			//Parameter for what stat you're looking for
+// 	  			data: {"window": "top_10_"+input},
+// 	  			success: function(stats){
+// 	  			console.log(stats.entries);
+// 	  			for(i =0; i<5; i++){
+// 	  				var values = {};
+// 	  				values = {
+// 	  					username: stats["entries"][i]["username"],
+// 	  					kills: stats["entries"][i]["kills"],
+// 	  					rank: stats["entries"][i]["rank"],
+// 	  					platform: stats["entries"][i]["platform"],
+// 	  					wins: stats["entries"][i]["wins"],
+// 	  					kd: stats["entries"][i]["kd"],
+// 	  					score: stats["entries"][i]["score"]
+// 	  				};
+// 	  				var lbstats = "<div class='lb-sub'> <div class='textBox'><h2>"+values.username+"</h1> <p>"+values[input].substring(0,1);
+// 	  				var lbInputstats = values[input]+"</p></div> <span><img src='/assets/img/platforms/"+values.platform+".png'></span></div>";
+// 	  				if(input === "kills"){
+// 	  					$(".lbKills").append(lbstats+lbInputstats);
+// 	  				} 
+// 	  				else if(input === "wins"){
+// 	  					$(".lbWins").append(lbstats+lbInputstats);
+// 	  				} 
+// 	  				else if(input === "score"){
+// 	  					$(".lbScore").append(lbstats+lbInputstats);
+// 	  				} 
+// 	  				else {
+// 	  					console.log("API does not detect that. Try checking the spelling")
+// 	  				}
+// 	  			}
+// 	  			},
+// 	  			error: function(jqXHR, textStatus, err){console.log(textStatus)}
+// 	  		})
+// 	}
+	// $.ajax({
+	// 	type: "POST",
+	// 	url: "https://fortnite-public-api.theapinetwork.com/prod09/br_motd/get",
+	// 	headers: APIKey,
+	// 	statsType: "json",
+	// 	data: {"language": "en"},
+	// 	success: function(data){
+	// 		console.log(data);
+	// 	},
+	// 	error: function(jqXHR, textStatus, err){
+	// 		console.log(err);
+	// 	}
+	// })
+var entries = [{
+            "title": "Make A Splash",
+            "body": "Chomp and Sky Stalker Gear in the Item Shop now!",
+            "image": "https://fortnite-public-files.theapinetwork.com/0b76799d416020eedcbe611364d2ee09.jpeg",
+            "time": "1539045123"
+        },
+        {
+            "title": "Quad Launcher",
+            "body": "Quickly fire up to 4 rockets to blanket an area with explosive damage.",
+            "image": "https://fortnite-public-files.theapinetwork.com/b5d8acc3d3b38ba1700ecd6958cc4616.jpeg",
+            "time": "1539014704"
+        },
+        {
+            "title": "Time For Harvest",
+            "body": "Straw Stuffed Gear available now!",
+            "image": "https://fortnite-public-files.theapinetwork.com/41ef6c17cae14a94b8d29b614b4eb679.jpeg",
+            "time": "1538870762"
+        }]
+function slideShow(){
+	var index = 0;
+	var prev = $(".prev");
+	var next = $(".next");
+	var jumbotron = $(".jumbotron");
+	prev.click(function(){
+		if(index<=0){
+			index+=2;
+			console.log(index);
+		} else{
+			index --;
+			console.log(index);
+		}
+		jumbotron.css("background-image", "url("+entries[index].image+")");
+		$(".jumbotron .description h1").text(entries[index].title);
+		$(".jumbotron .description p").text(entries[index].body);
+		if(!jumbotron.hasClass('fade')){
+    		jumbotron.addClass('fade');
+  		} 
+	});
+	next.click(function(){
+		if(index >= 2){
+			index-=2;
+		} else{
+			index++;
+			console.log(index);
+		}
+		$(".jumbotron").css("background-image", "url("+entries[index].image+")");
+		$(".jumbotron .description h1").text(entries[index].title);
+		$(".jumbotron .description p").text(entries[index].body);
+		if(!jumbotron.hasClass('fade')){
+    		jumbotron.addClass('fade');
+  		} else {
+    		jumbotron.removeClass('fade');
+  		} 
+	});
+
+	jumbotron.css("background-image", "url("+entries[index].image+")");
+	$(".jumbotron .description h1").text(entries[index].title);
+	$(".jumbotron .description p").text(entries[index].body);	
+}
+slideShow();
